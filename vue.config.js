94d6d5056db347293177694971043cd2e6ca0c95
zuobufan <2605331201@qq.com>
2021-12-02 09:37:09 +0800

    升级1.0.1版

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    publicPath:'./',
+    // publicPath: process.env.NODE_ENV === 'production'? '/网易严选/': '/',
+    assetsDir: 'static',
+    parallel: false,
+    publicPath: './',
     devServer:{
-        port:'626',
+        port:'80',
         open:true
     }
 }
+
